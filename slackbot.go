b7f3dc16fe97c108fc38f34047e4508d6f3982b8
Taylor Nation <tnation@linode.com>
2019-03-11 09:21:54 -0400

    Go FMT

--- slackbot.go
+++ slackbot.go
@@ -1,37 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "slackbot-go/util"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"slackbot-go/util"
 )
 
-func echoBody(w http.ResponseWriter, r *http.Request){
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Printf("[ERROR] %s", err)
-        return
-    }
+func echoBody(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return
+	}
 
-    w.WriteHeader(200)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(body)
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request){
-    w.WriteHeader(200)
-    w.Header().Set("Content-Type", "text/plain")
-    fmt.Fprintf(w, "This is the webserver root!")
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "This is the webserver root!")
 }
-
 
 func main() {
 
-
-    http.HandleFunc("/", routeutil.GETRoute(sayHello))
-    http.HandleFunc("/echo", routeutil.POSTRoute(echoBody))
-    log.Printf("Starting up")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", routeutil.GETRoute(sayHello))
+	http.HandleFunc("/echo", routeutil.POSTRoute(echoBody))
+	log.Printf("Starting up")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
